42281bafad51e2576b3f84096536dc1ceb43679a
Othmane Belhoussine <Othmane.belhou@gmail.com>
2020-11-02 17:03:17 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e1567c1e635a3688d3c2.js"
+    "url": "webpack-runtime-2ddd7e9b165d092be157.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0f972b041dded961f1ef50b9a91d2456"
+    "revision": "202ecb129aa618f7ccc86201fff373e6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
